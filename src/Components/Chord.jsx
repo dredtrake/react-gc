f4806c5d9869c94fa7a1c3ec15fa2b46588ba840
André Belhocine <dredtrake@gmail.com>
2019-09-14 19:34:12 +0200

    quick changes (syntaxe)

--- src/Components/Chord.jsx
+++ src/Components/Chord.jsx
@@ -10,15 +10,15 @@ const defaultProps = {
             { p: '3-2', f: true },
             { p: '4-2', f: true },
             { p: '5-0', f: false },
-        ]
-    }
+        ],
+    },
 };
 
 const Chord = () => {
     const { chord: { positions } } = defaultProps;
     return (
         <div className="chord">
-            { positions.map(item =>{
+            { positions.map(item => {
                 const c = item.f ? ' filled' : '';
                 return (<div className={`dot position-${item.p}${c}`} />);
             })}
