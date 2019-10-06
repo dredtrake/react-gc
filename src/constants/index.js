5a52718b9594993dcefffc6fae0ef8a22c201e08
dredtrake <dredtrake@gmail.com>
2019-10-07 00:55:25 +0200

    new chords, Gm is messing the thing up

--- src/constants/index.js
+++ src/constants/index.js
@@ -20,6 +20,28 @@ const Am = {
     ],
 };
 
+const B = {
+    name: 'B',
+    positions: [
+        { p: '1-2', f: true },
+        { p: '2-4', f: true },
+        { p: '3-4', f: true },
+        { p: '4-4', f: true },
+        { p: '5-2', f: true },
+    ],
+};
+
+const Bm = {
+    name: 'Bm',
+    positions: [
+        { p: '1-2', f: true },
+        { p: '2-3', f: true },
+        { p: '3-4', f: true },
+        { p: '4-4', f: true },
+        { p: '5-2', f: true },
+    ],
+};
+
 const C = {
     name: 'C',
     positions: [
@@ -32,10 +54,31 @@ const C = {
     ],
 };
 
+const Cm = {
+    name: 'Cm',
+    positions: [
+        { p: '1-1', f: true },
+        { p: '2-2', f: true },
+        { p: '3-3', f: true },
+        { p: '4-3', f: true },
+        { p: '5-1', f: true },
+    ],
+};
+
 const D = {
     name: 'D',
     positions: [
         { p: '1-2', f: true },
+        { p: '2-3', f: true },
+        { p: '3-2', f: true },
+        { p: '4-0', f: false },
+    ],
+};
+
+const Dm = {
+    name: 'Dm',
+    positions: [
+        { p: '1-1', f: true },
         { p: '2-3', f: true },
         { p: '3-2', f: true },
         { p: '4-0', f: false },
@@ -66,9 +109,57 @@ const Em = {
     ],
 };
 
+const F = {
+    name: 'F',
+    positions: [
+        { p: '1-1', f: false },
+        { p: '2-1', f: false },
+        { p: '3-2', f: true },
+        { p: '4-3', f: true },
+        { p: '5-3', f: true },
+        { p: '6-1', f: false },
+    ],
+};
+
+const Fm = {
+    name: 'Fm',
+    positions: [
+        { p: '1-1', f: false },
+        { p: '2-1', f: false },
+        { p: '3-1', f: false },
+        { p: '4-3', f: true },
+        { p: '5-3', f: true },
+        { p: '6-1', f: false },
+    ],
+};
+
+const G = {
+    name: 'G',
+    positions: [
+        { p: '1-3', f: true },
+        { p: '2-0', f: false },
+        { p: '3-0', f: false },
+        { p: '4-0', f: false },
+        { p: '5-2', f: true },
+        { p: '6-3', f: true },
+    ],
+};
+
+const Gm = {
+    name: 'Gm',
+    positions: [
+        { p: '1-3', f: true },
+        { p: '2-3', f: true },
+        { p: '3-3', f: true },
+        { p: '4-5', f: true },
+        { p: '5-5', f: true },
+        { p: '6-3', f: true },
+    ],
+};
+
 const none = {
     name: '',
     positions: [],
 }
 
-export default {A, Am, C, D, E, Em, none};
+export default {A, Am, B, Bm, C, Cm, D, Dm, E, Em, G, Gm, F, Fm, none};
